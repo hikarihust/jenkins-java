2f610176debd659330ce4d0c976e85b6c55bb70a
vudinhquang <vudinhquangk53@gmail.com>
2021-09-17 02:20:45 +0900

    jenkins java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello web java");
+		get("/hello", (req, res) -> "Hello jenkins java");
 	}
 }
